c973c0089277f36dc94be2cbd8164c87e5339fe5
pooneh <poonehshooshtari@gmail.com>
2018-05-24 17:28:27 -0600

    Completed the feature for creating random quote

--- _site/static/script.js
+++ _site/static/script.js
@@ -1,18 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
 
-	let newQuote;
-	let newQuoteRequest = new XMLHttpRequest();
-	newQuoteRequest.open('GET', 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1');
-	newQuoteRequest.onload = function(){
-		newQuote = JSON.parse(newQuoteRequest.responseText);	
-	};
-
-	newQuoteRequest.send();
-
+	let count = 0;
 	document.querySelector(".button").addEventListener("click", function(){
-		document.querySelector(".new_quote").innerHTML = newQuote[0].content;
-		document.querySelector(".author").innerHTML = newQuote[0].title;
-	});
-	
+		let newQuoteRequest = new XMLHttpRequest();
+		// To avoid caching we add a variable called count at the end of url
+		newQuoteRequest.open('GET', 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&klosuerdbfnvjmhb='+count);
+		count++;
+		newQuoteRequest.onload = function(){
+			let newQuote = JSON.parse(newQuoteRequest.responseText);
+			document.querySelector(".new_quote").innerHTML = newQuote[0].content;
+			document.querySelector(".author").innerHTML = newQuote[0].title;
+		};
+		newQuoteRequest.send();
+	});	
 });
 
