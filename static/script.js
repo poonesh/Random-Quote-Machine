4576919427ba34a59445472a15341270b9460698
pooneh <poonehshooshtari@gmail.com>
2018-05-24 15:26:39 -0600

    Parsed JSON and connected the button to the handler

--- static/script.js
+++ static/script.js
@@ -1,14 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
-	document.querySelector(".button").addEventListener("click", function(){
-		document.querySelector(".new_quote").innerHTML = "this is your new quote";
-	});
 
+	let newQuote;
 	let newQuoteRequest = new XMLHttpRequest();
-	newQuoteRequest.open('GET', 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=5');
+	newQuoteRequest.open('GET', 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1');
 	newQuoteRequest.onload = function(){
-		console.log(newQuoteRequest.responseText);
+		newQuote = JSON.parse(newQuoteRequest.responseText);	
 	};
 
 	newQuoteRequest.send();
+
+	document.querySelector(".button").addEventListener("click", function(){
+		document.querySelector(".new_quote").innerHTML = newQuote[0].content;
+		document.querySelector(".author").innerHTML = newQuote[0].title;
+	});
+	
 });
 
