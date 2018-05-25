8a0aed1131bedf83314343ed71606815306078d6
pooneh <poonehshooshtari@gmail.com>
2018-05-24 23:23:09 -0600

    Used jQuery to create random quotes and tweet out the code

--- _site/static/script.js
+++ _site/static/script.js
@@ -1,17 +1,39 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
 
-	let count = 0;
-	document.querySelector(".button").addEventListener("click", function(){
-		let newQuoteRequest = new XMLHttpRequest();
-		// To avoid caching we add a variable called count at the end of url
-		newQuoteRequest.open('GET', 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&klosuerdbfnvjmhb='+count);
-		count++;
-		newQuoteRequest.onload = function(){
-			let newQuote = JSON.parse(newQuoteRequest.responseText);
-			document.querySelector(".new_quote").innerHTML = newQuote[0].content;
-			document.querySelector(".author").innerHTML = newQuote[0].title;
-		};
-		newQuoteRequest.send();
-	});	
+	// let count = 0;
+	// document.querySelector(".newQuoteButton").addEventListener("click", function(){
+	// 	let newQuoteRequest = new XMLHttpRequest();
+	// 	// To avoid caching we add a variable called count at the end of url
+	// 	newQuoteRequest.open('GET', 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=1'+count);
+	// 	count++;
+	// 	newQuoteRequest.onload = function(){
+	// 		let newQuote = JSON.parse(newQuoteRequest.responseText);
+	// 		document.querySelector(".newQuote").innerHTML = newQuote[0].content;
+	// 		document.querySelector(".author").innerHTML = newQuote[0].title;
+	// 		console.log(newQuote[0].content);
+	// 		document.querySelector(".tweetShareLink").setAttribute("href", `https://twitter.com/share?text=${newQuote[0].content}`);
+	// 	};
+	// 	newQuoteRequest.send();
+	// });
+
+	document.querySelector(".newQuoteButton").addEventListener("click", function(){
+		$.ajax({
+			url : 'https://andruxnet-random-famous-quotes.p.mashape.com/',
+			type : 'GET',
+			success: function(data){
+				document.querySelector(".newQuote").innerHTML = data[0].quote;
+				document.querySelector(".author").innerHTML = data[0].author;
+				document.querySelector(".tweetShareLink").setAttribute("href", `https://twitter.com/share?text=${data[0].quote}`);
+			},
+
+			error: function(err){
+				console.log("errorrrrr!");
+			},
+
+			beforeSend: function(xhr){
+				xhr.setRequestHeader("X-Mashape-Key", "9VCED1Ft9Emshn5a3MPZeOUqWoEHp1noq21jsnDd6OYzyLV4jn")
+			}
+		});
+	});
 });
 
